5d85748447cf8d0431b4eab5d618a1956ec8c703
irvan1701 <39835355+irvan1701@users.noreply.github.com>
2023-05-31 20:30:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,16 +24,18 @@ with col1:
     cp = st.number_input('nyeri dada | 0(tidak ada), 1(Ringan), 2(Signifikan), dan 3(Hebat)', 0, 3)
 with col2:
     restecg = st.number_input('hasil ECG | 0(tidak ada kelainan), 1(kelainan minor), 2(kelainan signifikan)', 0, 2)
-
-fbs = st.number_input('konsentrasi gula setelah puasa dalam 8 jam | **1(> 120 mg/dl) dan 0(< 120 mg/dL)**', 0, 1)
-thalach = st.number_input('thalach (detak jantung maksimal setelah latihan fisik intens)', 0, 1000)
-exang = st.number_input('exang ( ada tidaknya rasa sakit atau tekanan pada dada akibat kurangnya pasokan darah dan oksigen yang cukup ke jantung)', 0, 1000)
 with col1:
     oldpeak = st.number_input('oldpeak (penurunan segmen ST pada elektrokardiogram (EKG) setelah latihan fisik)', 0, 1000)
 with col2:
     slope = st.number_input('slope (Tingkat penurunan ST segment dengan nilai 0(tidak ada), 1(lambat), 2(cepat))', 0, 2)
-ca = st.number_input('ca (jumlah sumbatan arteri koroner utama dengan nilai 0(tidak ada), 1(1 arteri), 2(2 arteri), 3(3 arteri)', 0, 3)
-thal = st.number_input('thal (aliran darah ke otot jantung dengan nilai 0 = normal, 1 = fixed defect, 2 = reversable defect)', 0, 2)
+with col1:
+    ca = st.number_input('ca (jumlah sumbatan arteri koroner utama dengan nilai 0(tidak ada), 1(1 arteri), 2(2 arteri), 3(3 arteri)', 0, 3)
+with col2:
+    thal = st.number_input('thal (aliran darah ke otot jantung dengan nilai 0 = normal, 1 = fixed defect, 2 = reversable defect)', 0, 2)
+fbs = st.number_input('konsentrasi gula setelah puasa dalam 8 jam | **1(> 120 mg/dl) dan 0(< 120 mg/dL)**', 0, 1)
+thalach = st.number_input('thalach (detak jantung maksimal setelah latihan fisik intens)', 0, 1000)
+exang = st.number_input('exang ( ada tidaknya rasa sakit atau tekanan pada dada akibat kurangnya pasokan darah dan oksigen yang cukup ke jantung)', 0, 1000)
+
 
 data = pd.read_csv('heart.csv')
 X = data.drop('target', axis=1)
